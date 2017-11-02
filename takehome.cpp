2a107c7d4fb335c4b16ee6bd212a6f725897f2c6
Prycrj <Prycrj@users.noreply.github.com>
2017-11-02 19:52:22 -0400

    Exam 2

---
+++ takehome.cpp
@@ -0,0 +1,104 @@
+/*
+//Name: Rayona Pryce
+/Creation: 11/2/2017
+//Version: Exam_2
+*/
+
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+//Section 1: Syntax
+
+
+// #1
+int negation(int n)
+{
+  if (n < 0)
+  { return n; }
+
+  else if (n >=0 || n==0)
+  { return n*-1; }
+}
+
+// #2
+int n;
+
+if ( n % 2 == 0 && n % 3==0)
+ { return 6; }
+
+ else if (n % 3 == 0)
+ { return 3; }
+
+ else if (n % 2 == 0)
+{ return 2; }
+
+ else
+{ return 1; }
+
+// #3
+string concatenate(string frst , string scnd)
+{
+  cout<<scnd<<first;
+}
+
+//#4
+float a,b,emphc;
+
+  if (a < b && a < emphc)
+  { return a; }
+  else if (b < a && b < emphc)
+  { return b; }
+  else
+  { return emphc; }
+}
+
+//#5
+int distance( int x , int y)
+{
+  if (x > y)
+  { return x - y; }
+  else
+  { return -1 * (x-y); }
+}
+
+//#6
+int s;
+if (s % 15 == 0)
+{ cout << "'To Fro'"; }
+
+else if (s %5 == 0)
+{ cout << ""Fro""; }
+
+else if (s % 3 == 0)
+{ cout << ""To""; }
+
+//#7
+int piecewise(int n)
+{
+  if (n > 0)
+  { return n + 5; }
+
+  else
+  { return (n*n) - 1; }
+}
+
+//#8
+void perimeter(double , double, double , double , double&);
+
+
+//Section 2: Debugging
+
+// line 1 -> int s
+// line 8 -> }
+// Line 9 -> int ts;
+// Line 21 -> "/n"
+// Line 22 -> "/n"
+// Line 23 -> return 0;
+
+int main ()
+{
+  return 0;
+}
